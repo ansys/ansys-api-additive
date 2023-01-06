5971c71626a681ce53e4b3a2aa52033748a1002a
Peter Krull <peter.krull@ansys.com>
2023-01-05 17:14:32 -0700

    Simplify material definition

--- run_protoc.sh
+++ run_protoc.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-outdir="protoc"
+outdir="protoc-output"
 rm -rf ${outdir}
 mkdir -p ${outdir}
 python -m grpc_tools.protoc --python_out=${outdir} --grpc_python_out=${outdir} -I. ansys/api/additive/v0/*.proto
