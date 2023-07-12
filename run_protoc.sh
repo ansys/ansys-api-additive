49fa1c1eb31b752abf13d5169e35ac0836f3e804
Peter Krull <peter.krull@ansys.com>
2023-07-12 16:17:22 -0600

    Fix about service naming

--- run_protoc.sh
+++ run_protoc.sh
@@ -4,4 +4,5 @@ rm -rf ${outdir}
 mkdir -p ${outdir}
 python -m grpc_tools.protoc --python_out=${outdir} --grpc_python_out=${outdir} -I. ansys/api/additive/v0/*.proto
 echo "protobuf interface files written to ${outdir}"
+dotnet clean csharp/*.csproj
 dotnet build csharp/*.csproj
