a3a9ff86b2953c325c5cfefa0f1bc8cd1961ed16
Peter Krull <peter.krull@ansys.com>
2023-07-12 15:17:23 -0600

    Add nuget package build to script

--- run_protoc.sh
+++ run_protoc.sh
@@ -4,3 +4,4 @@ rm -rf ${outdir}
 mkdir -p ${outdir}
 python -m grpc_tools.protoc --python_out=${outdir} --grpc_python_out=${outdir} -I. ansys/api/additive/v0/*.proto
 echo "protobuf interface files written to ${outdir}"
+dotnet build csharp/*.csproj
